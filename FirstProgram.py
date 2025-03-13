f116de66679d90ec223d5f3f1d49680f8e8a9790
himanshu101 <himanshukumar.connect@gmail.com>
2025-03-13 23:51:25 +0530

    apna college leacture

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,63 @@
-print("Hello world")
+# """ print("Hello world")
+# print(5+5)
+
+# #a variable is a name given to a memory location in a program
+
+# name = "Himanshu"
+# age = 23
+
+# print(type(age))
+# print(type(name))
+ 
+
+# a = 1000
+# b = 500
+# sum = a + b
+# print(sum)
+
+# """"""
+
+
+#punctuators are symbols to organize the sentence structure in programming.
+
+
+#Expression execution
+#string & numeric expression values can operate together with
+
+# A,B = 2,3
+# Txt="@"
+# print(2*Txt*3)
+
+#String  & String can operate with +
+
+# A,B = "2",3
+# Txt="@"
+# print((A+Txt)*B)
+
+
+#floor gives closest integer, which is lesser than or equal to the float value
+
+#resutl of (A//B) is same as floor(A/B)
+
+# A,B = 12,5
+# C = A/B
+# print(C)
+
+#Remainder is negative when denominator is negative
+
+# A, B = 5 ,-2 
+# C = A % B
+# print(C)
+
+#input() statement is used to accept values (using keyword) form user
+
+name = input("Enter your name:")
+
+print("Hello",name)
+
+#int input
+
+age = int(input("enter your age: "))
+
+print("Your age is",age)
+  
